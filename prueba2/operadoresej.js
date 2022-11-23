0018938f1a9ade030729bf1253a6057b7e33e372
Demian <53890358+demian35@users.noreply.github.com>
2022-11-23 02:41:41 -0600

    agregue mas operadores de asignacion ahora con resta , modulo y exponenciacion

--- prueba2/operadoresej.js
+++ prueba2/operadoresej.js
@@ -4,13 +4,24 @@ let numero=30
 
 numero+=30
 
+document.write("Suma de los numeros 30 + 30 mediante un incremento 30+=30 :  " + numero)
+// document.write() funcion para imprimir en la pagina algo
 
 //exponenciacion
 let numero2=54
 
 numero2**=3
 
-document.write("Suma de los numeros 30 + 30 mediante un incremento 30+=30 :  " + numero)
-// document.write() funcion para imprimir en la pagina algo
 
-document.write("\n 54 ")
+document.write("<br> 54 elevado al cubo es igual a: " + numero2)
+
+
+//resta
+let numero3=30
+numero3-=15
+document.write("<br> 30 -=13 es igual a : " + numero3)
+
+ //modulo
+ let numero4=35
+  numero4%=3
+  document.write("<br> 35 mod 3 es igual a : " +numero4) 
