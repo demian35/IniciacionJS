76312f4d6b8997b0009a5d5f6a4005fc0574354c
Demian <53890358+demian35@users.noreply.github.com>
2022-11-22 18:42:18 -0600

    Ejemplos de declaracion de variables en JS e imprimirlas en una pagina HTML

---
+++ prueba/intro.js
@@ -0,0 +1,22 @@
+
+
+//ejemplos de variables
+
+string="Hola mundo"
+Number=23
+bool= false
+
+//declaracion de variables
+const bote = "dinero"; // dato constante 
+
+let numero, numero2,numero3; //variable local para un bloque
+
+numero=29; //inicializacion de variable
+
+numero=30; //modificamos el valor
+numero2=4;
+numero3=8;
+
+alert(numero) //alert manda la variable en el navegador como notificacion
+alert(numero2)
+alert(numero3)
