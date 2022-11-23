c7de318dc2011384a39c2746e5085eefd1f86663
Demian <53890358+demian35@users.noreply.github.com>
2022-11-23 02:30:44 -0600

    ejercicio 2 operaciones basicas en js

---
+++ prueba2/operadoresej.js
@@ -0,0 +1,16 @@
+//operador para hacer incremento 
+
+let numero=30
+
+numero+=30
+
+
+//exponenciacion
+let numero2=54
+
+numero2**=3
+
+document.write("Suma de los numeros 30 + 30 mediante un incremento 30+=30 :  " + numero)
+// document.write() funcion para imprimir en la pagina algo
+
+document.write("\n 54 ")
