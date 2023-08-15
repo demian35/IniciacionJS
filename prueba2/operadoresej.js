305a47c779e070d0051ea28190955e4e96ef70f0
Demian <53890358+demian35@users.noreply.github.com>
2023-08-15 03:05:41 -0600

    corrigiendo una cadena

--- prueba2/operadoresej.js
+++ prueba2/operadoresej.js
@@ -19,7 +19,7 @@ document.write("<br> 54 elevado al cubo es igual a: " + numero2)
 //resta
 let numero3=30
 numero3-=15
-document.write("<br> 30 -=13 es igual a : " + numero3)
+document.write("<br> 30 - 15 es igual a : " + numero3)
 
  //modulo
 let numero4=35
