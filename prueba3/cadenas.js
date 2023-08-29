9bfc5944296cf0f73e5062621d1acb3c2e6276b2
Demian <53890358+demian35@users.noreply.github.com>
2023-08-29 02:55:01 -0600

    uso de ${} y concatenacion con +

--- prueba3/cadenas.js
+++ prueba3/cadenas.js
@@ -3,9 +3,20 @@
 cadena1="Hola";
 
 cadena2=" como estas mi nombre es ";
-
+//le pedimos al usuario que ingrese algo
 cadena3=prompt("ingrese un nombre:");
 
+//uso del metodo concat
 resulatado= cadena1.concat(cadena2,cadena3);
+//concatenacion tradicional
+resultado2= cadena1 + cadena2 + cadena3;
+
+//uso de literate templates para concatenacion de cadenas ${}
+const resultado3=`Hola ${cadena3} te gusta js?`
+
 
 document.write(resulatado);
+document.write("<br>")
+document.write(resultado2);
+document.write("<br>")
+document.write(resultado3);
