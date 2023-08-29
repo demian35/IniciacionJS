ff00ce66a10182b0ba4566fc0bc697bf5eda9a03
Demian <53890358+demian35@users.noreply.github.com>
2023-08-29 02:32:13 -0600

    Concatenando cadenas con concat()

---
+++ prueba3/cadenas.js
@@ -0,0 +1,11 @@
+
+//declarando cadenas con las que trabajare en esta leccion
+cadena1="Hola";
+
+cadena2=" como estas mi nombre es ";
+
+cadena3=prompt("ingrese un nombre:");
+
+resulatado= cadena1.concat(cadena2,cadena3);
+
+document.write(resulatado);
