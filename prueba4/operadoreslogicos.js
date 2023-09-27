6c5816692179561dc05d3b548c7b0394e1f5b6c6
Demian <53890358+demian35@users.noreply.github.com>
2023-09-26 23:58:09 -0600

    operaciones logicas

---
+++ prueba4/operadoreslogicos.js
@@ -0,0 +1,36 @@
+
+let v1=true;
+let v2=false;
+
+//operacion logica and
+let andresultado= v1 && v2;
+document.write("El resultado de v1 && v2 es: "+ andresultado);
+console.log(andresultado);
+
+
+document.write("<br>")
+
+//operacion logica or
+let orresultado= v1 || v2;
+document.write("El resultado de v1 v v2 es: "+ orresultado);
+console.log(orresultado);
+
+
+
+document.write("<br>")
+//negacion
+let v3=!true
+
+document.write("v3 negada es: "+ v3 );
+
+//comparaciones 
+
+let num1=23;
+let num2=43;
+let num3=56;
+let num4=67;
+
+let resulatado= (num2 < num1) && (num3 != num4);
+
+document.write("<br>");
+document.write(resulatado);
